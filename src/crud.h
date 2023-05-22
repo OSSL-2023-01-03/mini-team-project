8b17ffd782d743229e49f7546ff308b66b61781f
stevenCho <34876337+nerdbamboo@users.noreply.github.com>
2023-05-22 23:05:08 +0900

    [sungmin]다듬기
    약간의 코드 정리

--- src/crud.h
+++ src/crud.h
@@ -3,12 +3,6 @@
 
 #include <stdio.h>
 
-void read();
-void printList(int id);
-void addObj();
-void deleteObj();
-void updateObj();
-void loadData();
 void storeData();
 char rate();
 void bookTicket();
