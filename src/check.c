aac0a4f1f209a7806f64f4e81ff47a683606cf98
KwakNayeong <129664039+KwakNayeong@users.noreply.github.com>
2023-05-22 23:12:00 +0900

    [Nayeong]Create check.c

---
+++ src/check.c
@@ -0,0 +1,15 @@
+#include "check.h"
+#include <string.h>
+
+
+void display(int member){
+        char s1[10];
+        char st[10];
+        for(int i = 0; i < member; i++){
+                printf("좌석을 선택하세요.");
+                scanf("%s", st);
+                printf("%s확인\n", st);
+        }
+        printf("예약되었습니다.\n");
+
+}
