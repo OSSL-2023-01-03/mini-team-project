6b9d8441bd62fbba2677d807c1b50175d43e678c
KwakNayeong <129664039+KwakNayeong@users.noreply.github.com>
2023-05-22 22:41:18 +0900

    [Nayeong]Create seat.c

---
+++ src/seat.c
@@ -0,0 +1,27 @@
+#include "seat.h"
+
+void displaySeat(){
+        printf("****************\n");
+        printf("   스크린  \n");
+        printf("A1 ");
+        printf("A2 ");
+        printf("A3 ");
+        printf("A4 ");
+        printf("A5\n");
+        printf("B1 ");
+        printf("B2 ");
+        printf("B3 ");
+        printf("B4 ");
+        printf("B5\n");
+        printf("C1 ");
+        printf("C2 ");
+        printf("C3 ");
+        printf("C4 ");
+        printf("C5\n");
+        printf("D1 ");
+        printf("D2 ");
+        printf("D3 ");
+        printf("D4 ");
+        printf("D5\n");
+        printf("*****************\n");
+}
